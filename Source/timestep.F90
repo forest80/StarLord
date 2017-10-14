d799f437e4e251fc54750cef68ca2c6a4dcaa08c
Max Katz <maxpkatz@gmail.com>
2017-10-14 02:26:26 -0400

    Start ripping out from estdt

--- Source/timestep.F90
+++ Source/timestep.F90
@@ -13,13 +13,10 @@ contains
 #endif
   subroutine estdt(lo,hi,u,u_lo,u_hi,dx,dt)
 
-    use network, only: nspec, naux
     use eos_module, only: eos
     use eos_type_module, only: eos_t, eos_input_re
-    use bl_constants_module, only: ONE
-    use amrex_fort_module, only: rt => amrex_real, get_loop_bounds, amrex_min
-    use meth_params_module, only: NVAR, URHO, UMX, UMY, UMZ, UEINT, UTEMP, UFS, UFX
-    use prob_params_module, only: dim
+    use amrex_fort_module, only: rt => amrex_real, get_loop_bounds
+    use meth_params_module, only: NVAR
 
     implicit none
 
@@ -29,8 +26,7 @@ contains
     real(rt), intent(in   ) :: dx(3)
     real(rt), intent(inout) :: dt
 
-    real(rt) :: rhoInv, ux, uy, uz, c, dt1, dt2, dt3, dt_tmp
-    integer  :: i, j, k
+    integer  :: i, j
     integer  :: blo(3), bhi(3)
 
     type (eos_t) :: eos_state
@@ -39,50 +35,19 @@ contains
 
     ! Call EOS for the purpose of computing sound speed
 
-    do k = blo(3), bhi(3)
-       do j = blo(2), bhi(2)
-          do i = blo(1), bhi(1)
-             rhoInv = ONE / u(i,j,k,URHO)
+    blo(:) = 0
+    bhi(:) = 1
 
-             eos_state % rho = u(i,j,k,URHO )
-             eos_state % T   = u(i,j,k,UTEMP)
-             eos_state % e   = u(i,j,k,UEINT) * rhoInv
-             eos_state % xn  = u(i,j,k,UFS:UFS+nspec-1) * rhoInv
-             eos_state % aux = u(i,j,k,UFX:UFX+naux-1) * rhoInv
+    do j = blo(2), bhi(2)
+       do i = blo(1), bhi(1)
 
-             call eos(eos_input_re, eos_state)
+          eos_state % rho = 1.0d5
+          eos_state % T   = 1.0d8
+          eos_state % e   = 1.0d23
+          eos_state % xn  = 1.0d0
 
-             ! Compute velocity and then calculate CFL timestep.
+          call eos(eos_input_re, eos_state)
 
-             ux = u(i,j,k,UMX) * rhoInv
-             uy = u(i,j,k,UMY) * rhoInv
-             uz = u(i,j,k,UMZ) * rhoInv
-
-             c = eos_state % cs
-
-             dt1 = dx(1)/(c + abs(ux))
-             if (dim >= 2) then
-                dt2 = dx(2)/(c + abs(uy))
-             else
-                dt2 = dt1
-             endif
-             if (dim == 3) then
-                dt3 = dx(3)/(c + abs(uz))
-             else
-                dt3 = dt1
-             endif
-
-             dt_tmp = ONE/dt1
-             if (dim >= 2) then
-                dt_tmp = dt_tmp + ONE/dt2
-             endif
-             if (dim == 3) then
-                dt_tmp = dt_tmp + ONE/dt3
-             endif
-
-             call amrex_min(dt, ONE / dt_tmp)
-
-          enddo
        enddo
     enddo
 
